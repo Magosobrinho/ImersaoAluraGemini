63871b99fb71822d8611eca50860d25c3ccd844d
Magosobrinho <marianag1_s@hotmail.com>
2024-09-06 22:16:13 -0300

    quartaAula

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log(dados[2]);
+//console.log(dados);
+
+let section = document.getElementById("resultados-pesquisa");
+console.log(section);
+
+section.innerHTML = `
+    <div class="item-resultado">
+        <h2>
+            <a href="#" target="_blank">${dados[0].titulo}</a>
+        </h2>
+        <p class="descrição-meta">Ferramenta de otimização de processo seletivo</p> 
+        <a href="https://www.linkedin.com/jobs/vagas-em-brasília/?currentJobId=3993481293&originalSubdomain=br" target="_blank"> Saiba Mais </a>
+    </div>
+    `;
+
+
+
+
+
+
--- dados.js
+++ dados.js
@@ -1,12 +1,5 @@
 let dados = [
-    {
-        titulo: "Vagas",
-        descricao: "Nossa ferramenta de análise de processos seletivos oferece uma visão completa e detalhada de todo o funil",
-        link: "https://www.linkedin.com/jobs/vagas-em-bras%C3%ADlia/?currentJobId=3993481293&originalSubdomain=br"
-    },
-    {
-        titulo: "Candidatos",
-        descricao: "Esta ferramenta é excelente para otimizar o processo de recrutamento, pois atrai candidatos de alta qualidade. Com ela, você encontra rapidamente os profissionais mais qualificados, economizando tempo e recursos.",
-        link: "https://www.linkedin.com/jobs/vagas-em-bras%C3%lia/?currentJobId=3993481293&originalSubdomain=br"
-    }
+    "Vagas",
+    "Candidatos",
+    "Processo seletivo"
 ];
